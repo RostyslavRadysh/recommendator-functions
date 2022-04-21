0ebe0fe24cd3c65b6b86a92724f8f7f0db304cf4
firemeraldie <102985875+firemeraldie@users.noreply.github.com>
2022-04-21 10:08:53 +0300

    Add films command
    Added survey for favorite film genre and text instruction.

---
+++ src/commands/telegram/films/index.ts
@@ -0,0 +1,39 @@
+import { TelegramClient, TelegramTypes } from 'messaging-api-telegram'
+import TelegramMessage from '@/models/telegramMessage'
+import Command from '@/utils/command'
+
+class FilmsCommand implements Command {
+    regex: RegExp
+
+    constructor(private client: TelegramClient,) {
+        this.regex = new RegExp('/films')
+    }
+
+    async validate(body: any): Promise<boolean> {
+        const request = body as TelegramMessage
+        if (!request.message?.text) return false
+        if (!this.regex.test(request.message.text)) return false
+        if (!request.message.from?.id) return false
+        return true 
+    }
+
+    async execute(body: any): Promise<void> {
+        // Get the key
+        const key = (body as TelegramMessage).message?.from?.id
+        if (!key) throw Error('The key doesn\'t exists!')
+        // Send a message
+        const message = 'Choose your favourite film genre:'
+        const replyMarkup: TelegramTypes.ReplyKeyboardMarkup = {
+            keyboard: [
+                [{ text: 'Comedy' }, { text: 'Sci-fi' }],
+                [{ text: 'Horror' }, { text: 'Action' }],
+                [{ text: 'Drama' }, { text: 'Mystery' }],
+                [{ text: 'Thriller' }, { text: 'Other' }]
+            ],
+            resizeKeyboard: true
+        }
+        await this.client.sendMessage(key, message, { parseMode: TelegramTypes.ParseMode.HTML, replyMarkup })
+    }
+}
+
+export default FilmsCommand
